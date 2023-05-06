d46acc5c7be1cb3370d332ef1ef307b32c2bafb2
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2023-05-06 16:45:59 -0300

    Conexão com o Banco de dados MySql com Xampp e metados de abrir e fechar

---
+++ Conexao.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace ProjetoSistemas
+{
+    class Conexao
+    {
+        public string conec = "SERVER=localhost; DATABASE=aula; UID=root; PWD=; PORT=;"; //string de conexão
+
+        public MySqlConnection con = null;
+
+        public void AbrirConexao() //abrir conexao
+        {
+            //testar 
+            try
+            {
+                con = new MySqlConnection(conec);
+                con.Open();
+            }
+            catch(Exception ex)
+            {
+                //error
+                MessageBox.Show("Erro no Servidor!" + ex.Message);
+            }
+        }
+
+        public void FecharConexao() //fechar conexao
+        {
+            try
+            {
+                con = new MySqlConnection(conec);
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+                //error
+                MessageBox.Show("Erro no Servidor!" + ex.Message);
+            }
+        }
+
+
+    }
+
+}
